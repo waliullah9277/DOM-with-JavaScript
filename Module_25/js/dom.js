93bf127ee5b6760878e8d81248a81ab7d01bb0bb
Waliullah <waliullah9277>
2025-07-02 00:26:09 +0600

    dom with javascript

---
+++ Module_25/js/dom.js
@@ -0,0 +1,18 @@
+// console.log("Hello JavaScript");
+const liCollection = document.getElementsByTagName("li");
+for (const li of liCollection) {
+  // console.log(li.innerText)
+}
+
+const h1Collection = document.getElementsByTagName("h1");
+for (const h1 of h1Collection) {
+  // console.log(h1.innerText)
+}
+
+const domList = document.getElementsByClassName("dom-list");
+for (const d of domList) {
+  console.log(d.innerText);
+}
+
+const fruitsList = document.getElementsByClassName('fruits-list')
+console.log(fruitsList)
